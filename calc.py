7651aa5fa2512335d60fb6c2ca0ec2439b04f761
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-10-09 21:39:40 -0700

    fix misc bugs

--- calc.py
+++ calc.py
@@ -26,11 +26,13 @@ current_operator = None
 result_displayed = False
 
 def input_number(x):
-    global first_string, second_string, first_string_selected, result_displayed
+    print("number inputted")
+    global first_string, second_string, first_string_selected, result_displayed, has_decimal
     if result_displayed:
         first_string_selected = True
         result_displayed = False
         result = ""
+        has_decimal = False
     if not current_operator:
         first_string += str(x)
         num_label.config(text = first_string)
@@ -45,27 +47,37 @@ def input_decimal():
         has_decimal = True
 
 def input_operator(operator):
-    global current_operator, first_string, second_string, first_string_selected, result_displayed
-    
+    global current_operator, first_string, second_string, first_string_selected, result, result_displayed, has_decimal
+    print("operator inputted")
     # ignore this input if you hit an operator button before entering any input
     if first_string_selected and not first_string:
         print("attempted to enter an operator without entering a number first")
         return
 
-    if not current_operator:
-        current_operator = operator
-        first_string_selected = False
-    elif second_string:
+    if first_string == "." or second_string == ".":
+        print("attempted to enter just a decimal")
+        return
+    
+
+    
+    if second_string and current_operator:
         get_result()
+    if result_displayed:
         first_string = result
         result = ""
         result_displayed = False
+    if not current_operator:
         current_operator = operator
         first_string_selected = False
+        has_decimal = False
 
         
 def get_result():
-    global first_string, second_string, result, current_operator, result_displayed, first_string_selected
+    global first_string, second_string, result, current_operator, result_displayed, first_string_selected, has_decimal
+    print("getting result...")
+    if first_string == "." or second_string == ".":
+        print("attempted to enter just a decimal")
+        return
     global ADD, SUBTRACT, MULTIPLY, DIVIDE
     if first_string and second_string and current_operator:
         # damn you python. using magic numbers
@@ -83,16 +95,19 @@ def get_result():
         first_string = ""
         second_string = ""
         current_operator = None
+        print("result obtained:" + result)
 
 def clear():
-    global first_string, second_string, first_string_selected, result, result_displayed
+    global first_string, second_string, first_string_selected, result, result_displayed, has_decimal
     num_label.config(text = "")
     if result_displayed:
         result = ""
     elif first_string_selected:
         first_string = ""
+        has_decimal = False
     else:
         second_string = ""
+        has_decimal = False
 
 for i in range(10):
     buttons.append(ttk.Button(window, text = str(i), command = lambda j=i:input_number(j)))
